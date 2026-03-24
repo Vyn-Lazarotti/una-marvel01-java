8b31ff7511f2bd6d638c4481778c406410600682
Vinicius Lazarotti <viniciuslazarotti@gmail.com>
2026-03-24 19:42:44 -0300

    Alistamento.java

--- Alistamento.java
+++ Alistamento.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class Alistamento {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        String nome;
-        System.out.println("Digite seu nome: ");
-        nome = teclado.next();
+        
+        System.out.println("Digite o nome do recruta: ");
+        String nome = teclado.nextLine();
         System.out.println("Olá, " + nome + "!");
+
+        System.out.println("Digite o seu peso: ");
+        double peso = teclado.nextDouble();
+        System.out.println("Seu peso é " + peso + " kg");
+
+        if (peso < 50) {
+            System.out.println("Você está abaixo do peso ideal para o alistamento!");
+        } else if (peso >= 50 && peso <= 100) {
+            System.out.println("Você está dentro do peso ideal para o alistamento!");
+        } else {
+            System.out.println("Você está acima do peso ideal para o alistamento!");
 
         teclado.close();
     }
